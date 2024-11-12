ca0c8d1ba35712d94848bcdf2ca47c5f0b63c555
fuma-opening-line <168610315+fuma-opening-line@users.noreply.github.com>
2024-11-12 15:10:01 +0900

    Add typechain to dependencies

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,8 +1,15 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  typechain: {
+    outDir: 'types',
+    target: 'ethers-v6',
+  },
 };
 
 export default config;
