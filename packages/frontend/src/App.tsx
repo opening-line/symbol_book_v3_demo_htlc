634d03cba4a896a6cfa09a72522a3d1817813e85
fuma-opening-line <168610315+fuma-opening-line@users.noreply.github.com>
2024-11-14 17:54:06 +0900

    Add random number generation functionality

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,3 +1,28 @@
-export default function App() {
-  return <></>
+import { useState } from "react"
+
+export default function () {
+  const [payload, setPayload] = useState("")
+  return (
+    <>
+      <div>
+        <div>秘密を生成</div>
+        <div>
+          <textarea value={payload} disabled></textarea>
+          <button
+            onClick={() => {
+              const raw = new Uint8Array(1024)
+              crypto.getRandomValues(raw)
+              setPayload(
+                Array.from(raw)
+                  .map((b) => b.toString(16).padStart(2, "0"))
+                  .join(""),
+              )
+            }}
+          >
+            生成
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
