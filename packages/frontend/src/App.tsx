25ea259145fe7cdfc14f802e5dce700a2d2ac050
fuma-opening-line <168610315+fuma-opening-line@users.noreply.github.com>
2024-11-15 10:38:55 +0900

    Modularized components from App.tsx for improved structure

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,28 +1,12 @@
-import { useState } from "react"
+import { PayloadProvider } from "./context/Payload"
+import GeneratePayload from "./component/GeneratePayload"
 
 export default function () {
-  const [payload, setPayload] = useState("")
   return (
     <>
-      <div>
-        <div>秘密を生成</div>
-        <div>
-          <textarea value={payload} disabled></textarea>
-          <button
-            onClick={() => {
-              const raw = new Uint8Array(1024)
-              crypto.getRandomValues(raw)
-              setPayload(
-                Array.from(raw)
-                  .map((b) => b.toString(16).padStart(2, "0"))
-                  .join(""),
-              )
-            }}
-          >
-            生成
-          </button>
-        </div>
-      </div>
+      <PayloadProvider>
+        <GeneratePayload />
+      </PayloadProvider>
     </>
   )
 }
