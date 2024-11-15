f53b1da0cbbf09ab4eb7e99405d333a5eff3fc5b
fuma-opening-line <168610315+fuma-opening-line@users.noreply.github.com>
2024-11-15 11:13:27 +0900

    Add new context

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,16 +2,19 @@ import { EIP6963DetailProvider } from "./context/EIP6963Detail"
 import EIP6963Chooser from "./component/EIP6963Chooser"
 import { PayloadProvider } from "./context/Payload"
 import GeneratePayload from "./component/GeneratePayload"
+import { EthersBrowserProviderProvider } from "./context/EthersBrowserProvider"
 
 export default function () {
   return (
     <>
-      <EIP6963DetailProvider>
-        <EIP6963Chooser />
-      </EIP6963DetailProvider>
-      <PayloadProvider>
-        <GeneratePayload />
-      </PayloadProvider>
+      <EthersBrowserProviderProvider>
+        <EIP6963DetailProvider>
+          <EIP6963Chooser />
+        </EIP6963DetailProvider>
+        <PayloadProvider>
+          <GeneratePayload />
+        </PayloadProvider>
+      </EthersBrowserProviderProvider>
     </>
   )
 }
--- packages/frontend/src/component/EIP6963Chooser.tsx
+++ packages/frontend/src/component/EIP6963Chooser.tsx
@@ -1,18 +1,25 @@
 import { useContext } from "react"
 import { EIP6963DetailContext } from "../context/EIP6963Detail"
+import { EthersBrowserProviderContext } from "../context/EthersBrowserProvider"
+import { BrowserProvider } from "ethers"
 
 export default () => {
   const detail = useContext(EIP6963DetailContext)
+  const [_browserProvider, setBrowserProvider] = useContext(
+    EthersBrowserProviderContext,
+  )
   return (
     <>
       <div>EIP-6963対応ウォレット</div>
       {detail.map((d, i) => (
-        <label>
+        <label key={i}>
           <input
             type='radio'
             name='eip6963'
             value={i}
-            onChange={(_event) => {}}
+            onChange={(_event) => {
+              setBrowserProvider(new BrowserProvider(d.provider))
+            }}
           />
           {d.info.name}
         </label>
