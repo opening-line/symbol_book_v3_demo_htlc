297ca8f8a83f008e5705aec115801b262c4b00ba
fuma-opening-line <168610315+fuma-opening-line@users.noreply.github.com>
2024-11-18 10:54:44 +0900

    Add web UI for announcing SecretLockTransaction

--- packages/frontend/src/component/SecretLock.tsx
+++ packages/frontend/src/component/SecretLock.tsx
@@ -1,6 +1,15 @@
 import { keccak256 } from "ethers"
 import { useContext, useEffect, useState } from "react"
 import { PayloadContext } from "../context/Payload"
+import {
+  Address,
+  models,
+  descriptors,
+  Network,
+  SymbolFacade,
+  generateMosaicAliasId,
+} from "symbol-sdk/symbol"
+import { Hash256, PublicKey } from "symbol-sdk"
 
 export default () => {
   const [payload, _setPayload] = useContext(PayloadContext)
@@ -10,7 +19,13 @@ export default () => {
     setHash(keccak256(payload).substring(2).toUpperCase())
   }, [payload])
   const [value, setValue] = useState(1000000n)
-  const [time, setTime] = useState(0)
+  const [height, setHeight] = useState(0n)
+  if (
+    typeof (window as unknown as { requestSSS: unknown }).requestSSS ===
+    "function"
+  ) {
+    ;(window as unknown as { requestSSS: () => boolean }).requestSSS()
+  }
   return (
     <div>
       <div>SecretLockTransaction</div>
@@ -40,12 +55,12 @@ export default () => {
       </div>
       <div>
         <div>
-          ロックする期間
+          ロックする期間（ブロック）
           <input
             type='text'
-            value={time}
+            value={height.toString()}
             onChange={(event) => {
-              setTime(Number(event.target.value))
+              setHeight(BigInt(event.target.value))
             }}
           />
         </div>
@@ -62,7 +77,59 @@ export default () => {
           />
         </div>
       </div>
-      <button>アナウンス</button>
+      <button
+        onClick={(_event) => {
+          const facade = new SymbolFacade(Network.TESTNET)
+          const unsigned = facade.transactionFactory.create(
+            new descriptors.SecretLockTransactionV1Descriptor(
+              new Address(address),
+              new Hash256(hash),
+              new descriptors.UnresolvedMosaicDescriptor(
+                new models.UnresolvedMosaicId(
+                  generateMosaicAliasId("symbol.xym"),
+                ),
+                new models.Amount(value),
+              ),
+              new models.BlockDuration(height),
+              models.LockHashAlgorithm.SHA3_256,
+            ).toMap(),
+          )
+          unsigned.signerPublicKey = new models.PublicKey(
+            new PublicKey(
+              (window as unknown as { SSS: { activePublicKey: string } }).SSS
+                .activePublicKey,
+            ).bytes,
+          )
+          unsigned.fee = new models.Amount(BigInt(unsigned.size) * 100n)
+          unsigned.deadline = new models.Timestamp(
+            facade.now().addSeconds(45).timestamp,
+          )
+          ;(
+            window as unknown as {
+              SSS: { setTransactionByPayload: (_0: string) => void }
+            }
+          ).SSS.setTransactionByPayload(
+            Array.from(unsigned.serialize())
+              .map((b) => b.toString(16).padStart(2, "0"))
+              .join(""),
+          )
+          ;(
+            window as unknown as {
+              SSS: { requestSign: () => Promise<{ payload: string }> }
+            }
+          ).SSS.requestSign()
+            .then((r) => r.payload)
+            .then((r) => {
+              fetch(import.meta.env.VITE_SYMBOL_API_ORIGIN + "/transactions", {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ payload: r }),
+              })
+            })
+        }}
+      >
+        アナウンス
+      </button>
     </div>
   )
 }
