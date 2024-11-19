9d50868e7ca9f811f442ed3b8579f03d463994c7
fuma-opening-line <168610315+fuma-opening-line@users.noreply.github.com>
2024-11-19 14:19:39 +0900

    Add event

--- packages/contracts/contracts/HTLC.sol
+++ packages/contracts/contracts/HTLC.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.27;
 
 contract HTLC{
 
+    event Locked(address _from,address _to,uint256 _value,uint256 timelock,bytes32 hash);
+
     address payable immutable to;
     address payable immutable from;
     uint256 immutable value;
@@ -15,6 +17,8 @@ contract HTLC{
         to=payable(_to);
         timelock=_timelock;
         hash=_hash;
+
+        emit Locked(from,to,value,timelock,hash);
     }
 
     function redeem(bytes calldata preimage)external{
