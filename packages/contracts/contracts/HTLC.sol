c9c7ab4a4dd5cdff199df778b5fbc04e5f78700f
fuma-opening-line <168610315+fuma-opening-line@users.noreply.github.com>
2024-11-19 14:41:45 +0900

    Modify event signature to accept extra args

--- packages/contracts/contracts/HTLC.sol
+++ packages/contracts/contracts/HTLC.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.27;
 
 contract HTLC{
 
-    event Locked(address _from,address _to,uint256 _value,uint256 timelock,bytes32 hash);
+    event Locked(address _from,address _to,uint256 _value,uint256 timelock,bytes32 hash,bytes32 _to_symbol);
 
     address payable immutable to;
     address payable immutable from;
@@ -11,14 +11,14 @@ contract HTLC{
     uint256 immutable timelock;
     bytes32 immutable hash;
 
-    constructor(address _to,uint256 _timelock,bytes32 _hash)payable{
+    constructor(address _to,uint256 _timelock,bytes32 _hash,bytes32 _to_symbol)payable{
         value=msg.value;
         from=payable(msg.sender);
         to=payable(_to);
         timelock=_timelock;
         hash=_hash;
 
-        emit Locked(from,to,value,timelock,hash);
+        emit Locked(from,to,value,timelock,hash,_to_symbol);
     }
 
     function redeem(bytes calldata preimage)external{
