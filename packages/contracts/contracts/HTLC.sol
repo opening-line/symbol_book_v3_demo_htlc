34bb18e509865bcc706694b8a8a52ef66bd7d269
planethouki <26595148+planethouki@users.noreply.github.com>
2025-01-15 11:21:31 +0900

    space

--- packages/contracts/contracts/HTLC.sol
+++ packages/contracts/contracts/HTLC.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
-contract HTLC{
+contract HTLC {
 
-    event Locked(address _from,address indexed _to,uint256 _value,uint256 timelock,bytes32 hash,bytes _to_symbol);
-    event Redeemed(bytes32 hash,bytes _preimage);
+    event Locked(address _from, address indexed _to, uint256 _value, uint256 timelock, bytes32 hash, bytes _to_symbol);
+    event Redeemed(bytes32 hash, bytes _preimage);
 
     address payable immutable to;
     address payable immutable from;
@@ -12,25 +12,28 @@ contract HTLC{
     uint256 immutable timelock;
     bytes32 immutable hash;
 
-    constructor(address _to,uint256 _timelock,bytes32 _hash,bytes memory _to_symbol)payable{
-        value=msg.value;
-        from=payable(msg.sender);
-        to=payable(_to);
-        timelock=_timelock;
-        hash=_hash;
+    constructor(address _to, uint256 _timelock, bytes32 _hash, bytes memory _to_symbol) payable {
+        value = msg.value;
+        from = payable(msg.sender);
+        to = payable(_to);
+        timelock = _timelock;
+        hash = _hash;
 
-        emit Locked(from,to,value,timelock,hash,_to_symbol);
+        emit Locked(from, to ,value, timelock, hash, _to_symbol);
     }
 
-    function redeem(bytes calldata preimage)external{
-        require(sha256(abi.encodePacked(sha256(abi.encodePacked(preimage))))==hash);
-        require(block.timestamp<timelock);
+    function redeem(bytes calldata preimage) external {
+        require(sha256(abi.encodePacked(sha256(abi.encodePacked(preimage)))) == hash);
+        require(block.timestamp < timelock);
+
         to.transfer(value);
-        emit Redeemed(hash,preimage);
+
+        emit Redeemed(hash, preimage);
     }
 
-    function refund()external{
-        require(block.timestamp>=timelock);
+    function refund() external {
+        require(block.timestamp >= timelock);
+
         from.transfer(value);
     }
 }
