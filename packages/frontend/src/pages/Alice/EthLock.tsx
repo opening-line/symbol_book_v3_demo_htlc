2709017bfc06d5c4f78a5b76069da7eb15b0333d
planethouki <26595148+planethouki@users.noreply.github.com>
2025-01-16 20:10:52 +0900

    AliceEthLockをOfferだけに

--- packages/frontend/src/pages/Alice/EthLock.tsx
+++ packages/frontend/src/pages/Alice/EthLock.tsx
@@ -1,25 +1,15 @@
 import React from "react"
-import EIP6963Chooser from "../../component/EIP6963Chooser.tsx"
-import SSSChooser from "../../component/SSSChooser.tsx"
 import Offer from "../../component/Offer.tsx"
-import OfferReceived from "../../component/OfferReceived.tsx"
 
 export const AliceEthLock: React.FC = () => {
   return (
     <>
-      <h1>アリス</h1>
+      <h1>
+        アリス <small>ETHのロック</small>
+      </h1>
       <div className='grid'>
         <div>
-          <EIP6963Chooser />
-        </div>
-        <div>
-          <SSSChooser />
-        </div>
-        <div>
           <Offer />
-        </div>
-        <div>
-          <OfferReceived />
         </div>
       </div>
     </>
