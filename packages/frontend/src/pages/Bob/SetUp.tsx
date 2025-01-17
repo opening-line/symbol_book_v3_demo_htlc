7575b0b2da34269bdaef2127bc5882f058bcb6d0
planethouki <26595148+planethouki@users.noreply.github.com>
2025-01-17 18:19:03 +0900

    BobSetUp

--- packages/frontend/src/pages/Bob/SetUp.tsx
+++ packages/frontend/src/pages/Bob/SetUp.tsx
@@ -5,7 +5,7 @@ import SSSChooser from "../../component/SSSChooser.tsx"
 export const BobSetUp: React.FC = () => {
   return (
     <>
-      <h1>Bob</h1>
+      <h1>Bob <small>セットアップ</small></h1>
       <div className='grid'>
         <div>
           <EIP6963Chooser />
