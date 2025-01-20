f875b38f48551ce521ead676d32c1ca73dc1860b
planethouki <26595148+planethouki@users.noreply.github.com>
2025-01-20 19:44:06 +0900

    setupできたら次へ

--- packages/frontend/src/pages/Alice/SetUp.tsx
+++ packages/frontend/src/pages/Alice/SetUp.tsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react"
+import React, { useState, useMemo } from "react"
+import { useNavigate } from "react-router-dom"
 import EIP6963Chooser from "../../component/EIP6963Chooser.tsx"
 import SSSChooser from "../../component/SSSChooser.tsx"
 
 export const AliceSetUp: React.FC = () => {
+  const navigate = useNavigate()
   const [ethAddress, setEthAddress] = useState<string>("")
   const [symbolAddress, setSymbolAddress] = useState<string>("")
+
+  const buttonDisabled = useMemo(() => {
+    return ethAddress === "" || symbolAddress === ""
+  }, [ethAddress, symbolAddress])
+
+  const buttonClick = () => {
+    navigate("/alice/ethlock")
+  }
 
   return (
     <>
@@ -24,6 +34,11 @@ export const AliceSetUp: React.FC = () => {
             setSymbolAddress={setSymbolAddress}
           />
         </div>
+      </div>
+      <div>
+        <button type='button' onClick={buttonClick} disabled={buttonDisabled}>
+          次へ
+        </button>
       </div>
     </>
   )
--- packages/frontend/src/pages/Bob/SetUp.tsx
+++ packages/frontend/src/pages/Bob/SetUp.tsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react"
+import React, { useState, useMemo } from "react"
+import { useNavigate } from "react-router-dom"
 import EIP6963Chooser from "../../component/EIP6963Chooser.tsx"
 import SSSChooser from "../../component/SSSChooser.tsx"
 
 export const BobSetUp: React.FC = () => {
+  const navigate = useNavigate()
   const [ethAddress, setEthAddress] = useState<string>("")
   const [symbolAddress, setSymbolAddress] = useState<string>("")
+
+  const buttonDisabled = useMemo(() => {
+    return ethAddress === "" || symbolAddress === ""
+  }, [ethAddress, symbolAddress])
+
+  const buttonClick = () => {
+    navigate("/bob/ethlockwait")
+  }
 
   return (
     <>
@@ -24,6 +34,11 @@ export const BobSetUp: React.FC = () => {
             setSymbolAddress={setSymbolAddress}
           />
         </div>
+      </div>
+      <div>
+        <button type='button' onClick={buttonClick} disabled={buttonDisabled}>
+          次へ
+        </button>
       </div>
     </>
   )
