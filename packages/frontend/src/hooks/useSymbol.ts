79f17755c09b55d3b6285b11ab547af7f8b0901b
planethouki <26595148+planethouki@users.noreply.github.com>
2025-01-21 11:58:07 +0900

    alice xymunlock

--- packages/frontend/src/hooks/useSymbol.ts
+++ packages/frontend/src/hooks/useSymbol.ts
@@ -1,5 +1,3 @@
-import { useSecretProofContext } from "../context/SecretProofProvider.tsx"
-import { useMemo } from "react"
 import {
   Address,
   descriptors,
@@ -9,7 +7,6 @@ import {
 } from "symbol-sdk/symbol"
 import { Hash256, PublicKey } from "symbol-sdk"
 import {
-  getActiveAddress,
   getActivePublicKey,
   requestSign,
   setTransactionByPayload,
@@ -47,45 +44,17 @@ async function signSecretProofTransaction(unsigned: models.Transaction) {
 }
 
 async function announceSecretProofTransaction(signed: { payload: string }) {
-  await fetch(import.meta.env.VITE_SYMBOL_API_ORIGIN + "/transactions", {
+  return await fetch(import.meta.env.VITE_SYMBOL_API_ORIGIN + "/transactions", {
     method: "PUT",
     body: JSON.stringify({ payload: signed.payload }),
     headers: { "Content-Type": "application/json" },
   })
 }
 
-export default () => {
-  const { secret, proof } = useSecretProofContext()
-
-  const offerButtonDisabled = useMemo(() => {
-    return proof === "" || secret === ""
-  }, [proof, secret])
-
-  const onButtonClick = async () => {
-    const activeAddress = getActiveAddress()
-    const [unsigned] = createSecretProofTransaction(
-      proof,
-      activeAddress,
-      secret,
-    )
-    const [signed] = await signSecretProofTransaction(unsigned)
-    await announceSecretProofTransaction(signed)
+export function useSecretProofTransaction() {
+  return {
+    createSecretProofTransaction,
+    signSecretProofTransaction,
+    announceSecretProofTransaction,
   }
-
-  return (
-    <div>
-      <h2>お取引を受諾</h2>
-      <div>
-        <div>
-          <button
-            type='button'
-            disabled={offerButtonDisabled}
-            onClick={onButtonClick}
-          >
-            提案を受ける
-          </button>
-        </div>
-      </div>
-    </div>
-  )
 }
--- packages/frontend/src/pages/Alice/XymUnlock.tsx
+++ packages/frontend/src/pages/Alice/XymUnlock.tsx
@@ -1,7 +1,52 @@
-import React from "react"
-import UnlockXym from "../../component/UnlockXym.tsx"
+import React, { useMemo, useState } from "react"
+import { getActiveAddress } from "sss-module"
+import { useSecretProofContext } from "../../context/SecretProofProvider.tsx"
+import { useSecretProofTransaction } from "../../hooks/useSymbol.ts"
+import { Link } from "react-router-dom"
 
 export const AliceXymUnlock: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
+  const [result, setResult] = useState<string>("")
+  const [txHash, setTxHash] = useState<string>("")
+
+  const { secret, proof } = useSecretProofContext()
+  const {
+    createSecretProofTransaction,
+    signSecretProofTransaction,
+    announceSecretProofTransaction,
+  } = useSecretProofTransaction()
+
+  const offerButtonDisabled = useMemo(() => {
+    return proof === "" || secret === "" || isLoading
+  }, [proof, secret, isLoading])
+
+  const nextButtonDisabled = useMemo(() => {
+    return txHash !== ""
+  }, [txHash])
+
+  const onOfferButtonClick = async () => {
+    setIsLoading(true)
+    try {
+      const activeAddress = getActiveAddress()
+      const [unsigned] = createSecretProofTransaction(
+        proof,
+        activeAddress,
+        secret,
+      )
+      const [signed] = await signSecretProofTransaction(unsigned)
+      setTxHash(signed.hash)
+      const announceResult = await announceSecretProofTransaction(signed).then(
+        (res) => res.json(),
+      )
+      setResult(JSON.stringify(announceResult))
+    } catch (e) {
+      setError(JSON.stringify(e))
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <>
       <h1>
@@ -9,8 +54,84 @@ export const AliceXymUnlock: React.FC = () => {
       </h1>
       <div className='grid'>
         <div>
-          <UnlockXym />
+          <h2>お取引を受諾</h2>
+          <h3>情報</h3>
+          <div
+            style={{
+              marginTop: 10,
+              wordBreak: "break-all",
+              fontFamily: "monospace",
+            }}
+          >
+            シークレット： {secret}
+          </div>
+          <div
+            style={{
+              marginTop: 10,
+              wordBreak: "break-all",
+              fontFamily: "monospace",
+            }}
+          >
+            プルーフ： {proof}
+          </div>
+          <div style={{ marginTop: 20 }}>
+            <div>
+              <button
+                type='button'
+                disabled={offerButtonDisabled}
+                onClick={onOfferButtonClick}
+              >
+                {isLoading ? "解除中..." : "解除する"}
+              </button>
+            </div>
+          </div>
+          <h3>結果</h3>
+          {txHash && (
+            <div
+              style={{
+                marginTop: 10,
+                wordBreak: "break-all",
+                fontFamily: "monospace",
+              }}
+            >
+              トランザクションハッシュ： {txHash}
+            </div>
+          )}
+          {error && (
+            <div
+              style={{
+                marginTop: 10,
+                wordBreak: "break-all",
+                fontFamily: "monospace",
+              }}
+            >
+              エラー： {error}
+            </div>
+          )}
+          {result && (
+            <div
+              style={{
+                marginTop: 10,
+                wordBreak: "break-all",
+                fontFamily: "monospace",
+              }}
+            >
+              アナウンスの結果： {result}
+            </div>
+          )}
         </div>
+      </div>
+      <div>
+        {nextButtonDisabled && (
+          <button type='button' disabled={true}>
+            次へ
+          </button>
+        )}
+        {!nextButtonDisabled && (
+          <Link className='button' to='/alice/finish'>
+            次へ
+          </Link>
+        )}
       </div>
     </>
   )
