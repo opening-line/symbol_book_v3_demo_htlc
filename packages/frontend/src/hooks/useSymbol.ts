b41fb6a3b5b24453ec32974cd22db9762b047289
planethouki <26595148+planethouki@users.noreply.github.com>
2025-01-21 14:33:00 +0900

    bob xymlock

--- packages/frontend/src/hooks/useSymbol.ts
+++ packages/frontend/src/hooks/useSymbol.ts
@@ -1,6 +1,7 @@
 import {
   Address,
   descriptors,
+  generateMosaicAliasId,
   models,
   Network,
   SymbolFacade,
@@ -51,10 +52,73 @@ async function announceSecretProofTransaction(signed: { payload: string }) {
   })
 }
 
+function createSecretLockTransaction(
+  secret: string,
+  recipient: string,
+  timeLock: number,
+) {
+  const facade = new SymbolFacade(Network.TESTNET)
+  const transaction = facade.createTransactionFromTypedDescriptor(
+    new descriptors.SecretLockTransactionV1Descriptor(
+      new Address(recipient),
+      new Hash256(secret),
+      new descriptors.UnresolvedMosaicDescriptor(
+        new models.UnresolvedMosaicId(generateMosaicAliasId("symbol.xym")),
+        new models.Amount(1000000n),
+      ),
+      new models.BlockDuration(
+        BigInt(
+          Math.floor(
+            (Number(timeLock) * 1000 - new Date().valueOf()) / 1000 / 30 - 1,
+          ),
+        ),
+      ),
+      models.LockHashAlgorithm.HASH_256,
+    ),
+    new PublicKey(
+      (
+        window as unknown as {
+          SSS: { activePublicKey: string }
+        }
+      ).SSS.activePublicKey,
+    ),
+    100,
+    60,
+    0,
+  )
+  return [transaction]
+}
+
+async function signSecretLockTransaction(unsigned: models.Transaction) {
+  setTransactionByPayload(
+    Array.from<number>(unsigned.serialize())
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join(""),
+  )
+  const signed = await requestSign()
+  return [signed]
+}
+
+async function announceSecretLockTransaction(signed: { payload: string }) {
+  return await fetch(import.meta.env.VITE_SYMBOL_API_ORIGIN + "/transactions", {
+    method: "PUT",
+    body: JSON.stringify({ payload: signed.payload }),
+    headers: { "Content-Type": "application/json" },
+  })
+}
+
 export function useSecretProofTransaction() {
   return {
     createSecretProofTransaction,
     signSecretProofTransaction,
     announceSecretProofTransaction,
+  }
+}
+
+export function useSecretLockTransaction() {
+  return {
+    createSecretLockTransaction,
+    signSecretLockTransaction,
+    announceSecretLockTransaction,
   }
 }
--- packages/frontend/src/pages/Bob/XymLock.tsx
+++ packages/frontend/src/pages/Bob/XymLock.tsx
@@ -1,7 +1,52 @@
-import React from "react"
-import OfferXym from "../../component/OfferXym.tsx"
+import React, { useMemo, useState } from "react"
+import { useSecretProofContext } from "../../context/SecretProofProvider.tsx"
+import { useEthereumContractProvider } from "../../context/EthereumContractProvider.tsx"
+import { useSecretLockTransaction } from "../../hooks/useSymbol.ts"
+import { Link } from "react-router-dom"
 
 export const BobXymLock: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
+  const [result, setResult] = useState<string>("")
+  const [txHash, setTxHash] = useState<string>("")
+
+  const { secret, recipient } = useSecretProofContext()
+  const { timeLock } = useEthereumContractProvider()
+  const {
+    createSecretLockTransaction,
+    signSecretLockTransaction,
+    announceSecretLockTransaction,
+  } = useSecretLockTransaction()
+
+  const buttonDisabled = useMemo(() => {
+    return secret === "" || recipient === "" || timeLock === 0 || isLoading
+  }, [secret, recipient, timeLock])
+
+  const nextButtonDisabled = useMemo(() => {
+    return txHash === ""
+  }, [txHash])
+
+  const onButtonClick = async () => {
+    try {
+      const [unsigned] = createSecretLockTransaction(
+        secret,
+        recipient,
+        timeLock,
+      )
+      const [signed] = await signSecretLockTransaction(unsigned)
+      setTxHash(signed.hash)
+      const announceResult = await announceSecretLockTransaction(signed).then(
+        (res) => res.json(),
+      )
+      setResult(JSON.stringify(announceResult))
+    } catch (e) {
+      console.error(e)
+      setError(JSON.stringify(e))
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <>
       <h1>
@@ -9,8 +54,91 @@ export const BobXymLock: React.FC = () => {
       </h1>
       <div className='grid'>
         <div>
-          <OfferXym />
+          <h2>お取引のご提案</h2>
+          <h3>情報</h3>
+          <div
+            style={{
+              marginTop: 10,
+              wordBreak: "break-all",
+              fontFamily: "monospace",
+            }}
+          >
+            シークレット： {secret}
+          </div>
+          <div
+            style={{
+              marginTop: 10,
+              wordBreak: "break-all",
+              fontFamily: "monospace",
+            }}
+          >
+            アリスのアドレス： {recipient}
+          </div>
+          <div
+            style={{
+              marginTop: 10,
+              wordBreak: "break-all",
+              fontFamily: "monospace",
+            }}
+          >
+            期限： {new Date(timeLock * 1000).toLocaleString()} (unixtime: {timeLock})
+          </div>
+          <div style={{marginTop: 10}}>
+            <button
+              type='button'
+              onClick={onButtonClick}
+              disabled={buttonDisabled}
+            >
+              {isLoading ? "ご提案中..." : "ご提案"}
+            </button>
+          </div>
+          <h3>結果</h3>
+          {txHash && (
+            <div
+              style={{
+                marginTop: 10,
+                wordBreak: "break-all",
+                fontFamily: "monospace",
+              }}
+            >
+              トランザクションハッシュ： {txHash}
+            </div>
+          )}
+          {error && (
+            <div
+              style={{
+                marginTop: 10,
+                wordBreak: "break-all",
+                fontFamily: "monospace",
+              }}
+            >
+              エラー： {error}
+            </div>
+          )}
+          {result && (
+            <div
+              style={{
+                marginTop: 10,
+                wordBreak: "break-all",
+                fontFamily: "monospace",
+              }}
+            >
+              アナウンスの結果： {result}
+            </div>
+          )}
         </div>
+      </div>
+      <div>
+        {nextButtonDisabled && (
+          <button type='button' disabled={true}>
+            次へ
+          </button>
+        )}
+        {!nextButtonDisabled && (
+          <Link className='button' to='/bob/xymunlockwait'>
+            次へ
+          </Link>
+        )}
       </div>
     </>
   )
