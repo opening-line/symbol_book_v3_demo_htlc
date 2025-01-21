a939773b706b086a0be629564d7a89c48f466082
planethouki <26595148+planethouki@users.noreply.github.com>
2025-01-21 11:28:55 +0900

    alice xymlockwait 途中

--- packages/frontend/src/pages/Alice/XymLockWait.tsx
+++ packages/frontend/src/pages/Alice/XymLockWait.tsx
@@ -1,7 +1,59 @@
-import React from "react"
-import OfferXymWait from "../../component/OfferXymWait.tsx"
+import React, {useState} from "react"
+import { useEffect } from "react"
+import { getActiveAddress } from "sss-module"
+import { useSecretProofContext } from "../../context/SecretProofProvider.tsx"
 
 export const AliceXymLockWait: React.FC = () => {
+  const [txHash, setTxHash] = useState<string>("")
+
+  const activeAddress = getActiveAddress()
+  const { secret } = useSecretProofContext()
+
+  useEffect(() => {
+    const url = import.meta.env.VITE_SYMBOL_API_ORIGIN
+    const ws = new WebSocket(url.replace(/^http/, "ws") + "/ws")
+    let uid: string | undefined = undefined
+    const topic = "confirmedAdded/" + activeAddress
+    ws.onmessage = async (event) => {
+      if (typeof event.data !== "string") {
+        return
+      }
+      const eventData = JSON.parse(event.data)
+      if (eventData.uid !== undefined) {
+        uid = eventData.uid
+        ws.send(
+          JSON.stringify({
+            uid,
+            subscribe: "block",
+          }),
+        )
+        ws.send(
+          JSON.stringify({
+            uid,
+            subscribe: topic,
+          }),
+        )
+      }
+      if (uid === undefined) {
+        return
+      }
+      if (eventData.topic == topic) {
+        console.log(eventData)
+        if (
+          eventData.data.transaction.secret == secret &&
+          eventData.data.transaction.amount == "1000000"
+        ) {
+          setTxHash(eventData.data.meta.hash)
+          ws.close()
+        }
+      }
+    }
+
+    return () => {
+      ws.close()
+    }
+  }, [])
+
   return (
     <>
       <h1>
@@ -9,7 +61,8 @@ export const AliceXymLockWait: React.FC = () => {
       </h1>
       <div className='grid'>
         <div>
-          <OfferXymWait />
+          <h2>ボブのXYMロック</h2>
+          <p>{txHash}</p>
         </div>
       </div>
     </>
